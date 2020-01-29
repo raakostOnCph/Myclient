e2d1f0ef3049ca1a225017c9ff7cc068fb1b1b5e
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2020-01-29 14:27:57 +0100

    nu med parameter

--- MyClient.java
+++ MyClient.java
@@ -8,7 +8,7 @@ import java.io.*;
 
 class MyClient{
 public static void main(String args[])throws Exception{
-Socket s=new Socket("localhost",3333);
+Socket s=new Socket("localhost",Integer.parseInt(args[0]));
 DataInputStream din=new DataInputStream(s.getInputStream());
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
